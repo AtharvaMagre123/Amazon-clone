69612c4b6c099bc9bb8d16e4cd67a48426b32faf
Atharva Raju Magre <atharvamagre12@gmail.com>
2024-10-16 15:55:50 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVlrNv0VIgHCBzgrxFneGVKPXrwtO1USM",
+    authDomain: "amazn-fb392.firebaseapp.com",
+    projectId: "amazn-fb392",
+    storageBucket: "amazn-fb392.appspot.com",
+    messagingSenderId: "999076028242",
+    appId: "1:999076028242:web:f7429c3e1d63796e39e48b"
+  };
