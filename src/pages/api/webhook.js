0096450222ca1f59ac93199890b8215f9d184ca7
ATHARVA MAGRE <110712067+AtharvaMagre123@users.noreply.github.com>
2024-10-29 15:21:51 +0530

    Update webhook.js

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,7 +1,7 @@
 import { buffer } from "micro";
 import * as admin from "firebase-admin";
 
-const serviceAccount = require('../../../permissions.json');
+//const serviceAccount = require('../../../permissions.json');
 
 const fulfillOrder = async (session) => {
   return app
